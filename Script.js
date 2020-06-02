911c7e1e9b88dfb93b47be057fe513f0ab43576b
Conner Jarrell <connerjarrell07@gmail.com>
2020-06-01 20:46:45 -0400

    adding my questions to the script

--- Script.js
+++ Script.js
@@ -1,11 +1,46 @@
 var quizContainer = document.getElementById('quiz');
 var resutsContiner = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+var myQuestions = [
+    {
+      question: "Who won the first SuperBowl?",
+      answers: {
+        a: "Kansas City Chiefs",
+        b: "Chicago Bears",
+        c: "Green Bay Packers"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Who has the most touchdowns thrown in NFL history?",
+      answers: {
+        a: "",
+        b: "",
+        c: ""
+      },
+      correctAnswer: ""
+    },
+    {
+      question: "How many players are on a NFL roster?",
+      answers: {
+        a: "",
+        b: "j",
+        c: "",
+        d: ""
+      },
+      correctAnswer: "d"
+    }
+];
 
-function buildQuiz(){}
+function buildQuiz(){
+    var output = [];
+
+    myQuestions.forEach
+}
 
 function showResults(){}
 
 buildQuiz();
 
 submitButton.addEventListener('click' , showResults);
+
