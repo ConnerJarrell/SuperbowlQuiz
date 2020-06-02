b976f54ea8acf6d6295ba31e105c9b65ea5850ff
Conner Jarrell <connerjarrell07@gmail.com>
2020-06-01 20:24:45 -0400

    adding HTML and Script files

---
+++ Script.js
@@ -0,0 +1,11 @@
+var quizContainer = document.getElementById('quiz');
+var resutsContiner = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+function buildQuiz(){}
+
+function showResults(){}
+
+buildQuiz();
+
+submitButton.addEventListener('click' , showResults);
